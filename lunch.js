3410874a2b354c0671c024c372839413ab110462
Mariahhm42 <134189527+Mariahhm42@users.noreply.github.com>
2024-10-13 00:24:51 -0600

    lunch.js

--- lunch.js
+++ lunch.js
@@ -1,16 +1,14 @@
 const whatToDoForLunch = function(hungry, availableTime) {
-  console.log("I don't know what to do!");
   if (!hungry)
-    console.log("Go back to work, you're not hungry!")
-  
+    console.log("Go back to work, you're not hungry!");
   else 
     if (availableTime < 20)
-      console.log ("Pick something else and eat it in the lab")
-  
+      console.log ("Pick something else and eat it in the lab");
+
     else if (availableTime >= 20 && availableTime <= 30)
-      console.log("Try a new place nerarby")
+      console.log("Try a new place nearby")
     else {
-        console.log ("You're in a Bootcamp, reconsider how much time ou have to spare")
+        console.log ("You're in a Bootcamp, reconsider how much time you have to spare");
       }
 };
 
@@ -29,18 +27,15 @@ console.log("---");
 console.log("I'm hungry and I have 15 minutes for lunch.");
 whatToDoForLunch(true, 15);
 
-//Output I'm hungry and I have 20 minutes for lunch.
-// I don't know what to do!
-// Try a new place nerarby
+//Output 
+// I'm hungry and I have 20 minutes for lunch.
+// Try a new place nearby
 // ---
 // I'm hungry and I have 50 minutes for lunch.
-// I don't know what to do!
-// You're in a Bootcamp, reconsider how much time ou have to spare
+// You're in a Bootcamp, reconsider how much time you have to spare
 // ---
 // I'm not hungry and I have 30 minutes for lunch.
-// I don't know what to do!
-// Go back to work, you're not hungr!
+// Go back to work, you're not hungry!
 // ---
 // I'm hungry and I have 15 minutes for lunch.
-// I don't know what to do!
 // Pick something else and eat it in the lab
